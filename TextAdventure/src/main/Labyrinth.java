2bacd26323483b4537042cd17a62cf7416e7d368
EmilUndMaik <45424243+EmilUndMaik@users.noreply.github.com>
2018-11-28 14:18:23 +0100

    Add files via upload

--- TextAdventure/src/main/Labyrinth.java
+++ TextAdventure/src/main/Labyrinth.java
@@ -1,29 +1,47 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Labyrinth {
 	private Room startRoom;
 	private Room centralRoom;
-	private Room northRoom;
-	private Room eastRoom;
-	private Room southRoom;
+	
 	private Room currentRoom;
+	
+	
 	private GuiController guiController;
-
+	
+	private List<Room> dungeon = new ArrayList<>();
+	
+	
+	private Random random = new Random();
+	
+	
+	private void Door() {
+		if random < 7 ) {
+			newRoom = new Room("Neuer Raum!!!", null, null, null, null, x++, y++);
+		}
+	}
+	
+	
+	
 	public Labyrinth(GuiController guiController) {
 		this.guiController = guiController;
 		
-		centralRoom = new Room("Als dudurch die Tür kommst, siehst du einen weiteren leeren und mit Fackeln mäßig erhellten  Raum. Vier  Türen befinden sich an den Wänden.", null, null, null, null);
-		northRoom = new Room("Die Wände bestehen aus grob behauenen Sein,Wasser tropft überlaut von der Decke... Ein wildes Grollen ertönt in der Ferne. Doch ansonsten ist der Raum leer...", null, null, centralRoom, null);
-		eastRoom = new Room("Ein Kamin in der Ecke könnte Wärme und Licht spenden, doch der Schonstein ist eingestürzt. Es bleibt dunkel und nasskalt...", null, null, null, centralRoom);
-		southRoom = new Room("Du spürst einen Luftzug,  als du den Raum betrittst und voller Vorfreude stürmst du hinein... Doch alles, was du entdecken kannst ist ein Spalt in der felsigen Wand. Zu eng, um sich hindurchzuzwängen...", centralRoom,  null,  null,  null);
-		startRoom  = new Room("Du befindest dich in einer düsteren Höhle. An den Seitenwänden flackert der Schein zahlloser Fackel. Es gibt nur einen Weg: Und der ist vorwärts.", null, centralRoom,  null, null);
 		
-		centralRoom.setEast(eastRoom);
-		centralRoom.setNorth(northRoom);
-		centralRoom.setSouth(southRoom);
+		System.out.println(random.nextInt(10));
+		
+		
+		
+		
+		centralRoom = new Room("Als dudurch die Tür kommst, siehst du einen weiteren leeren und mit Fackeln mäßig erhellten  Raum. Vier  Türen befinden sich an den Wänden.", null, null, null, null, 0, 0);
+		
+		
 		centralRoom.setWest(startRoom);
 		
-		changeRoom(startRoom);
+		changeRoom(centralRoom);
 	}
 	
 	public void changeRoom(Room room) {
--- TextAdventure/src/main/Room.java
+++ TextAdventure/src/main/Room.java
@@ -6,17 +6,22 @@ public class Room {
 	private Room east;
 	private Room south;
 	private Room west;
+	private int x;
+	private int y;
+	
 	
 	public Room() {
 		description = "Dieser Raum ist öde und leer.";
 	}
 	
-	public Room(String description, Room north, Room east, Room south, Room west) {
+	public Room(String description, Room north, Room east, Room south, Room west, int x, int y) {
 		this.description = description;
 		this.north = north;
 		this.east = east;
 		this.south = south;
 		this.west = west;
+		this.x = x;
+		this.y = y;
 	}
 	
 	public String getDescprition() {
@@ -38,6 +43,14 @@ public class Room {
 	public void setWest(Room west) {
 		this.west = west;
 	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
 
 	public Room getEastRoom() {
 		return east;
@@ -53,5 +66,13 @@ public class Room {
 	
 	public Room getNorthRoom() {
 		return north;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
 	}
 }
